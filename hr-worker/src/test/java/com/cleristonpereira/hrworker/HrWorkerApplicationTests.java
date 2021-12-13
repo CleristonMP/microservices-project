e4fd60b325f8dfb38ebef10840ebc3fd7fdb9a53
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-12-13 16:14:18 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/cleristonpereira/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleristonpereira.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
