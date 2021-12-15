b5d4e34c284e7a0bda265bff89242bb5a5d42234
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-12-15 10:57:17 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/cleristonpereira/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleristonpereira.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
