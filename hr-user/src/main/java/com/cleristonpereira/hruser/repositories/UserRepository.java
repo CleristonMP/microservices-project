5f5ffae1bbff825833197c2451ce8c8c6bf18891
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-12-16 15:14:21 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/cleristonpereira/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cleristonpereira.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cleristonpereira.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
